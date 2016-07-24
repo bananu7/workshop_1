c99908ac93408ce5812e507b14327fdedaf464bd
bananu7 <bananu7@o2.pl>
2016-07-24 14:57:50 +0200

    Added proper structures to store the parsed data

--- Szkolenie_1/main.cpp
+++ Szkolenie_1/main.cpp
@@ -1,30 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+
+struct Vertex {
+    float x, y, z;
+};
+
+struct Face {
+    std::vector<unsigned> vertices;
+};
+
+struct Model {
+    std::vector<Vertex> vertices;
+    std::vector<Face> faces;
+};
 
 int main(int argc, const char* argv[]) {
-    std::ifstream f("teapot.obj");
+    std::ifstream file("teapot.obj");
     
-    if (!f) {
+    if (!file) {
         std::cerr << "Error opening file";
         std::exit(1);
     }
     
     std::string line;
-    unsigned faceCount = 0, vertCount = 0;
-    while(std::getline(f, line)) {
+    
+    Model model;
+    
+    while(std::getline(file, line)) {
         std::istringstream s(line);
         
         char c;
         s >> c;
         
         switch (c) {
-            case 'f': faceCount += 1; break;
-            case 'v': vertCount += 1; break;
+            case 'v':
+                Vertex vertex;
+                s >> vertex.x >> vertex.y >> vertex.z;
+                model.vertices.push_back(vertex);
+                
+                break;
+                
+            case 'f':
+                unsigned vertexNum;
+                Face face;
+                
+                while (s >> vertexNum) {
+                    face.vertices.push_back(vertexNum);
+                }
+                
+                model.faces.push_back(face);
+                
+                break;
         }
     }
     
-    std::cout << "Vertices: " << vertCount << '\n'
-              << "Faces: " << faceCount << '\n';
+    std::cout << "Vertices: " << model.vertices.size() << '\n'
+              << "Faces: " << model.faces.size() << '\n';
     
 }
